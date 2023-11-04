50f813df1d21f8ddf9f5846b94187916e7658874
gus3000 <gus3000spam@gmail.com>
2023-11-04 23:27:31 +0100

    fix: tests

--- tests/Feature/QuestionTest.php
+++ tests/Feature/QuestionTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use Database\Seeders\QuizSeeder;
+use Database\Seeders\UserSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -11,8 +12,9 @@ class QuestionTest extends TestCase
     use RefreshDatabase;
 
     public function test_question_exists(): void {
+        $this->seed(UserSeeder::class);
         $this->seed(QuizSeeder::class);
 
-        $this->assertDatabaseCount('questions', 20);
+        $this->assertDatabaseCount('questions', 40);
     }
 }
