f9a91d7d9b0fb9419264ff0aae64be1519092194
gus3000 <gus3000spam@gmail.com>
2023-11-15 16:29:19 +0100

    feat: add default label for submitted quizzes

--- resources/js/Pages/Dashboard/SubmittedQuizzes.vue
+++ resources/js/Pages/Dashboard/SubmittedQuizzes.vue
@@ -8,6 +8,7 @@ import {PlusIcon} from "flowbite-vue-icons";
 import { Link } from '@inertiajs/vue3';
 import {onMounted} from "vue";
 import {initFlowbite} from "flowbite";
+import Text from "@/Components/Text.vue";
 
 const props = defineProps<{
     quizzes: TQuiz[]
@@ -19,6 +20,7 @@ const props = defineProps<{
         <div class="p-6 text-gray-900 dark:text-gray-100 gap-4">
             <div class="divide-y divide-gray-200 dark:divide-gray-700 flex flex-col items-center">
                 <QuizRecap v-for="quiz in quizzes" :quiz="quiz" class="h-10 w-80"/>
+                <Text v-if="!quizzes.length">Pas de quiz proposé pour l'instant !</Text>
             </div>
         </div>
     </DashboardLayout>
