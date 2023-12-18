d9d40b8d3e195749959d80d5936322f91e989c1e
gus3000 <gus3000spam@gmail.com>
2023-12-18 10:19:54 +0100

    wip: add chart bar to stats

---
+++ resources/js/Components/Chart/PieBar.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import {Bar} from "vue-chartjs"
+import {BarElement, CategoryScale, Chart as ChartJS, Legend, LinearScale, Title, Tooltip} from 'chart.js'
+import {ref, computed} from "vue";
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
+
+const chartOptions = ref({
+  responsive: true,
+});
+
+const props = defineProps({
+  chartData: Array,
+});
+
+const chartData = computed(() =>
+{
+  return {
+    labels: ['January', 'February', 'March'],
+    datasets: [{data: props.chartData}]
+  };
+});
+;
+</script>
+
+<template>
+<Bar
+id="pouet"
+:options="chartOptions"
+:data="chartData"
+/>
+</template>
+
+<style scoped>
+
+</style>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
 import DashboardLayout from "@/Layouts/DashboardLayout.vue";
+import {Bar} from 'vue-chartjs'
+import {computed, ref} from "vue";
+import LabelButton from "@/Components/Button/LabelButton.vue";
+import PieBar from "@/Components/Chart/PieBar.vue";
+
+const a = ref(40);
+const b = ref(20);
+const c = ref(12);
+
+const chartData = computed((a,b,c) => [a,b,c]);
 </script>
 
 <template>
     <DashboardLayout title="Statistiques">
         <div class="p-6 text-gray-900 dark:text-gray-100">insérer Statistiques ici</div>
+        <LabelButton text="Increment" @click="" />
+        <PieBar
+            id="user-stats"
+            :chartData="chartData"
+        />
     </DashboardLayout>
 </template>
